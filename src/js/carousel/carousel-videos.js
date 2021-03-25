b6a68050cef298d6949b39129920678dee0cad4a
Aleksey <alex.kobychev@ontico.ru>
2021-03-25 21:26:12 +0300

    video slides indication

--- src/js/carousel/carousel-videos.js
+++ src/js/carousel/carousel-videos.js
@@ -1,4 +1,18 @@
-$("#carousel-videos").slick({
+const $slideStatus = $('#carousel-indicates');
+const $slickElement = $('#carousel-videos');
+
+$slickElement.on(
+  'init reInit afterChange',
+  function (event, slick, currentSlide, nextSlide) {
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    let i = (currentSlide ? currentSlide : 0) + 1;
+    $slideStatus.html(
+      `${i} <span class="color-grey">/ ${slick.slideCount}</span>`
+    );
+  }
+);
+
+$slickElement.slick({
   fade: true,
   dots: false,
   infinite: false,
@@ -19,7 +33,7 @@ $("#carousel-videos").slick({
   // settings: "unslick"
   // instead of a settings object
   // ],
-  appendArrows: $("#videos-nav"),
-  nextArrow: $("#video-next"),
-  prevArrow: $("#video-prev"),
+  appendArrows: $('#videos-nav'),
+  nextArrow: $('#video-next'),
+  prevArrow: $('#video-prev'),
 });
